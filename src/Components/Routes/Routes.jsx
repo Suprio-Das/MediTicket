5409f3b92ce87d09a239ef9a967c7a4d3a387f4e
Suprio-Das <priyamd507@gmail.com>
2025-05-09 14:24:18 +0600

    Main layout added to the route

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <h1>This is root.</h1>
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <h1>This is children of root.</h1>
+            }
+        ]
     }
 ])
 
