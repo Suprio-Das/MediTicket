db4b77fe655253c8291219d4bfb30217a69e5e93
Suprio-Das <priyamd507@gmail.com>
2025-05-10 22:44:49 +0600

    Navbar navigations added

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -12,27 +12,20 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Ticket</a></li>
+                            <li><a>Stats</a></li>
+                            <li><a>Doctors</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">
+                    <a className="text-xl">
                         <img src={MediTicketLogo} alt="MediTicketLogo" className='w-42' />
                     </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Ticket</a></li>
+                        <li><a>Stats</a></li>
+                        <li><a>Doctors</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
