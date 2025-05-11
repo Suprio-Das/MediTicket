d3057b8301c36f91d44aa21acf1d33bee80579db
Suprio-Das <priyamd507@gmail.com>
2025-05-11 21:49:32 +0600

    Location icon added

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router';
 import MediTicketLogo from '../../assets/MediTicket.png';
 import { IoMdMailUnread } from "react-icons/io";
 import { MdCall } from "react-icons/md";
+import { IoLocation } from "react-icons/io5";
 const Navbar = () => {
     return (
         <div>
             <div className='bg-amber-500 p-2 flex justify-center gap-5'>
                 <p className='flex items-center gap-0.5'><IoMdMailUnread className='mt-0.5' /> mediticket@gmail.com</p>
                 <p className='flex items-center gap-0.5'><MdCall /> 01610243377</p>
-                <p>Chattogram, Bangladesh</p>
+                <p className='flex items-center gap-0.5'><IoLocation /> Chattogram, Bangladesh</p>
             </div>
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start">
