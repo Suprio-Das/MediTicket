cc9e2d914b6ae0b34ec4ae7fb0c57a9b77c0f1fd
Suprio-Das <priyamd507@gmail.com>
2025-05-12 16:30:54 +0600

    Notice section flexed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             {/* Notice Board */}
             <div>
-                <div className="bg-gray-200 p-2">
+                <div className="bg-gray-200 p-2 flex items-center">
                     <button className="btn bg-[#275596] outline-0 text-white">Notice: </button>
                     <Marquee>
                         I can be a React component, multiple React components, or just some text.
