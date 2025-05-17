8cde0d3b86894a4993b3e87f3f31eea6627c6f76
Suprio-Das <priyamd507@gmail.com>
2025-05-17 21:11:16 +0600

    Ticket basic info added

--- src/Components/Home/Ticket.jsx
+++ src/Components/Home/Ticket.jsx
@@ -6,6 +6,12 @@ const Ticket = () => {
             {/* Header Section */}
             <div>
                 <img src={MediTicketLogo} alt="MediTicketLogo" className='w-8' />
+                <div>
+                    <h1>MediTicket Govt. Medical</h1>
+                    <p className='text-xs'>Chattogram, Bangladesh</p>
+                    <p className='text-xs'>Phone: 01610-2433** Fax: 031-610022</p>
+                    <p className='text-xs'>Email: mediticket@gmail.com Website: mediticket.gov.bd</p>
+                </div>
             </div>
         </div>
     );
