394e72dc4a465846fec8a4aa6683eb5dae7060e6
Suprio-Das <priyamd507@gmail.com>
2025-06-03 22:06:39 +0600

    Email and password fetched to the function

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,7 +2,10 @@ import Logo from '../../assets/MediTicket.png';
 const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault();
-        alert('Handle Login Function Clicked');
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.table(email, password);
     }
     return (
         <div className='min-h-[calc(100vh-120px)] flex items-center justify-center login border-t-1 border-[#275596]'>
@@ -14,11 +17,11 @@ const Login = () => {
                 <form onSubmit={handleLogin}>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Email</legend>
-                        <input type="text" className="input w-full" placeholder="Enter email..." />
+                        <input type="text" className="input w-full" name='email' placeholder="Enter email..." />
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Password</legend>
-                        <input type="password" className="input w-full" placeholder="Type password..." />
+                        <input type="password" className="input w-full" name='password' placeholder="Type password..." />
                     </fieldset>
                     <input type="submit" className='btn w-full mt-2 bg-[#275596] text-white' value="Login" />
                 </form>
