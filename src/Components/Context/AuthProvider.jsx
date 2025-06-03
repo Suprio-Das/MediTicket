422efee34e8a65bf11d96bf52d2d40feefb880e0
Suprio-Das <priyamd507@gmail.com>
2025-06-03 14:33:42 +0600

    user added to the context value

--- src/Components/Context/AuthProvider.jsx
+++ src/Components/Context/AuthProvider.jsx
@@ -3,7 +3,9 @@ import AuthContext from './AuthContext';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const authInfo = {};
+    const authInfo = {
+        user
+    };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
