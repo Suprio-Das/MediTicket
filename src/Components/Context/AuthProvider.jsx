03c76a91b2d9b4ffad593747b7288a4ec228d283
Suprio-Das <priyamd507@gmail.com>
2025-06-03 14:35:12 +0600

    loading state declared

--- src/Components/Context/AuthProvider.jsx
+++ src/Components/Context/AuthProvider.jsx
@@ -3,6 +3,7 @@ import AuthContext from './AuthContext';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const authInfo = {
         user
     };
