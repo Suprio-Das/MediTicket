47d5b5981054b07bf484cf3a27c7a398de4733f0
Suprio-Das <priyamd507@gmail.com>
2025-06-03 14:44:28 +0600

    Login form layout designed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,18 @@
+import Lottie from 'lottie-react';
+import loginLottie from '../../assets/LoginLottie.json';
 const Login = () => {
     return (
         <div>
-            <h1>This is login page.</h1>
+            <div>
+                {/* Lottie Animation */}
+                <div>
+                    <Lottie animationData={loginLottie} className='w-78'></Lottie>
+                </div>
+                {/* Login Form */}
+                <div>
+                    <h1>Login Form</h1>
+                </div>
+            </div>
         </div>
     );
 };
