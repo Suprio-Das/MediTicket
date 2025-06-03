fc785ee0af9e06f7b04ac7ac17948a6d404a922a
Suprio-Das <priyamd507@gmail.com>
2025-06-03 14:47:15 +0600

    Parent layout flexed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,11 +2,11 @@ import Lottie from 'lottie-react';
 import loginLottie from '../../assets/LoginLottie.json';
 const Login = () => {
     return (
-        <div>
+        <div className='min-h-[calc(100vh-85px)] flex items-center justify-center'>
             <div className='grid grid-cols-1 lg:grid-cols-2 items-center'>
                 {/* Lottie Animation */}
                 <div>
-                    <Lottie animationData={loginLottie} className='w-78'></Lottie>
+                    <Lottie animationData={loginLottie} className='w-[500px]'></Lottie>
                 </div>
                 {/* Login Form */}
                 <div>
