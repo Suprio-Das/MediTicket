f1eb1b0ba8aa23f9a77c253cf2fed4fa04d461bf
Suprio-Das <priyamd507@gmail.com>
2025-06-03 23:55:51 +0600

    Private routes private

--- src/Components/Routes/PrivateRoutes.jsx
+++ src/Components/Routes/PrivateRoutes.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import { Navigate } from "react-router";
+import AuthContext from "../Context/AuthContext";
 
-const PrivateRoutes = () => {
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    if (user) {
+        return children;
+    }
     return (
         <Navigate to='/login'></Navigate>
     );
