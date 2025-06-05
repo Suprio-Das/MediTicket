0ac60331c15f4f9afb313f83bf5dcde11795bfec
Suprio-Das <priyamd507@gmail.com>
2025-06-05 19:54:17 +0600

    Ticket verification route created

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/verification',
+                element: <h1>Verification page.</h1>
             }
         ]
     }
