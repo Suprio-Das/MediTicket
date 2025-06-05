1fb14f520e22e507ed1c0705e500f56870fbad45
Suprio-Das <priyamd507@gmail.com>
2025-06-05 20:04:14 +0600

    Verfied badge added

--- src/Components/TicketVerification/TicketVerification.jsx
+++ src/Components/TicketVerification/TicketVerification.jsx
@@ -1,7 +1,14 @@
+import Lottie from "lottie-react";
+import VerifiedBadge from '../../assets/VerifiedBadge.json';
+
 const TicketVerification = () => {
     return (
-        <div>
-            <h1>Verification Component!</h1>
+        <div className="min-h-[calc(100vh-120px)] flex items-center justify-center">
+            <div>
+                <div>
+                    <Lottie animationData={VerifiedBadge} className="w-21"></Lottie>
+                </div>
+            </div>
         </div>
     );
 };
