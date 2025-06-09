0b2e62bfc5df53b724f877376053f734ef27162a
Suprio-Das <priyamd507@gmail.com>
2025-06-09 20:01:37 +0600

    Medicine department title added

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -1,7 +1,15 @@
 const Doctors = () => {
     return (
         <div className="w-[95%] mx-auto">
+            <section>
+                {/* Medicine Department */}
+                <h1 className="lg:text-2xl text-xl font-semibold">Medicine Department</h1>
+                <div>
+                    <div>
 
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
