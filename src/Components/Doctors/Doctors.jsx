cb58f92082397ca0361e89f3a0cd72330a3f68a0
Suprio-Das <priyamd507@gmail.com>
2025-06-09 20:09:04 +0600

    Page title added

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -1,6 +1,7 @@
 const Doctors = () => {
     return (
         <div className="w-[95%] mx-auto doctors">
+            <h1 className="text-center text-3xl font-semibold my-5">Our Doctors</h1>
             <section>
                 {/* Medicine Department */}
                 <h1 className="lg:text-2xl text-xl font-semibold">Medicine Department</h1>
