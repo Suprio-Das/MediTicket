34710c52ffd6d9bbc4493ae6630c79c45de8e0d1
Suprio-Das <priyamd507@gmail.com>
2025-06-10 19:19:56 +0600

    Doctors page subtitle added

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -2,6 +2,7 @@ const Doctors = () => {
     return (
         <div className="mx-auto doctors p-5">
             <h1 className="text-center text-3xl font-semibold">Our Doctors</h1>
+            <p className="text-center">Our team is made up of caring professionals who deliver high-quality services.</p>
             <section>
                 {/* Medicine Department */}
                 <h1 className="lg:text-2xl text-xl font-semibold">Medicine Department</h1>
