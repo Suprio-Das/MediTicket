19415714d0567d68c9f4884bf789765393937578
Suprio-Das <priyamd507@gmail.com>
2025-06-10 18:48:40 +0600

    Doctors div modified

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -1,7 +1,7 @@
 const Doctors = () => {
     return (
-        <div className="w-[95%] mx-auto doctors">
-            <h1 className="text-center text-3xl font-semibold my-5">Our Doctors</h1>
+        <div className="mx-auto doctors p-5">
+            <h1 className="text-center text-3xl font-semibold">Our Doctors</h1>
             <section>
                 {/* Medicine Department */}
                 <h1 className="lg:text-2xl text-xl font-semibold">Medicine Department</h1>
