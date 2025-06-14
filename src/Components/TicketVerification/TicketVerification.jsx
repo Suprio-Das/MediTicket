50813ea21329be162b25ee96bcd469211730a949
Suprio-Das <priyamd507@gmail.com>
2025-06-14 21:13:41 +0600

    TicketVerification logic set to dynamic

--- src/Components/TicketVerification/TicketVerification.jsx
+++ src/Components/TicketVerification/TicketVerification.jsx
@@ -1,18 +1,26 @@
 import Lottie from "lottie-react";
 import VerifiedBadge from '../../assets/VerifiedBadge.json';
-import { useLoaderData } from "react-router";
+import NotValid from '../../assets/NotValid.json';
+import { useLoaderData, useParams } from "react-router";
 
 const TicketVerification = () => {
     const data = useLoaderData();
-    console.log(data);
+    const params = useParams();
+    const visitedRegNo = parseInt(params.regNo);
     return (
         <div className="min-h-[calc(100vh-120px)] flex items-center justify-center">
             <div>
                 <div className="flex items-center justify-center">
-                    <Lottie animationData={VerifiedBadge} className="w-21"></Lottie>
+                    {
+                        (data.regNo === visitedRegNo) ? <Lottie animationData={VerifiedBadge} className="w-21"></Lottie> : <Lottie animationData={NotValid} className="w-21"></Lottie>
+                    }
                 </div>
                 <div>
-                    <h1>This Ticket is the official product of MediTicket.</h1>
+                    <h1>
+                        {
+                            (data.regNo === visitedRegNo) ? 'This Ticket is the official product of MediTicket.' : 'This Ticket seems an unofficial product. Please contact helpline.'
+                        }
+                    </h1>
                 </div>
             </div>
         </div>
